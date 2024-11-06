900e1e326ec0174bca832266be399ba732c41783
Isabelle Deslé <isabelle.desle@student.howest.be>
2024-11-06 21:19:09 +0100

    readme & MySqlScript

---
+++ Database_CreateDb_Tables.sql
@@ -0,0 +1,37 @@
+-- Create the database
+CREATE DATABASE VirtualGlobeTrotter;
+
+-- Use the database
+USE VirtualGlobeTrotter;
+
+-- Create the Music table with the Tag column as VARCHAR(999)
+CREATE TABLE Music (
+    MusicID INT AUTO_INCREMENT PRIMARY KEY,
+    Title VARCHAR(255) NOT NULL,
+    Artist VARCHAR(255) NOT NULL,
+    Album VARCHAR(255),
+    ReleaseYear INT,
+    Tag VARCHAR(999)
+);
+
+-- Create the Image table with the Tag column as VARCHAR(999)
+CREATE TABLE Image (
+    ImageID INT AUTO_INCREMENT PRIMARY KEY,
+    FilePath VARCHAR(255) NOT NULL,
+    Description TEXT,
+    Tag VARCHAR(999)
+);
+
+-- Create the third table that connects Music and Image
+CREATE TABLE MusicImage (
+    MusicImageID INT AUTO_INCREMENT PRIMARY KEY,
+    MusicID INT,
+    ImageID INT,
+    FOREIGN KEY (MusicID) REFERENCES Music(MusicID),
+    FOREIGN KEY (ImageID) REFERENCES Image(ImageID)
+);
+
+
+INSERT INTO Image (FilePath, Description, Tag)
+VALUES 
+('https://github.com/IsabelleDesle/VirtualGlobeTrotter/blob/main/IMG_20241106_145012.jpg', 'Aligre', 'Paris, city, church');
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # VirtualGlobeTrotter
 Team Project Virtual GlobeTrotter
+
+added 
+presentation version 1 
+gitignore 
+example photo to provide path in database
+sql script

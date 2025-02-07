b2e1e3b64e79ea21d6ac2d9956d4f3aecdba3139
Isabelle Deslé <146006993+IsabelleDesle@users.noreply.github.com>
2025-02-07 16:55:09 +0100

    Update Constants.py

--- AI_Atvars/different_model_inference/Constants.py
+++ AI_Atvars/different_model_inference/Constants.py
@@ -1,1 +1,1 @@
-HUGGINGFACE_API_KEY = "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" # You need to generate an API key from Hugging Face, and place it in a Constants.py file in this folder
+HUGGINGFACE_API_KEY = "hf_nnf" # You need to generate an API key from Hugging Face, and place it in a Constants.py file in this folder
